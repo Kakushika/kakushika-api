b3eb1f4bd30087e29215f3670f2016443e096d64
Orleika <orleika.net@gmail.com>
2015-07-27 02:10:17 +0900

    コメントの除去

--- app.js
+++ app.js
@@ -13,16 +13,6 @@ var express = require('express'),
   Sequelize = require('sequelize'),
   config = require('./config');
 
-// var sequelize = new Sequelize(config.db.database, config.db.username, config.db.password, {
-//   host: config.db.host,
-//   dialect: config.db.dialect,
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     idle: 10000
-//   },
-// });
-
 // Use helmet to secure Express headers
 app.use(helmet());
 
