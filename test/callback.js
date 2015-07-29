b33cf487266946736dffeac72b3ef127b6d16bcd
vagrant <vagrant@localhost.localdomain>
2015-07-29 16:37:16 +0000

    テストコードの追加

---
+++ test/callback.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var should = require('chai').should(),
+  expect = require('chai').expect,
+  supertest = require('supertest'),
+  app = require('../app');
+
+describe('Callback', function() {
+  it('should return a 302 response', function(done) {
+    supertest(app)
+      .get('/callback/slack')
+      .expect(302)
+      .end(function(err, res) {
+        if (err) return done(err);
+        expect(res.header).to.have.property('location');
+        expect(res.header.location).not.to.be.null;
+        done();
+      });
+  });
+});
