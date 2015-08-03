09bef79e332ae1467bc0aa27547ac6bfd3129fd8
Orleika <orleika.net@gmail.com>
2015-08-04 03:16:46 +0900

    issue #14 salt&hash, (stretching)に対応

--- controllers/signup.js
+++ controllers/signup.js
@@ -3,8 +3,21 @@
 var express = require('express'),
   router = express.Router(),
   validator = require('validator'),
+  bcrypt = require('bcrypt'),
   config = require('config'),
   models = require('../models');
+
+var getPasswordHash = function(password, cb) {
+  bcrypt.genSalt(config.login.password.cost, function(err, salt) {
+    bcrypt.hash(password, salt, function(err, hash) {
+      if (err) {
+        cb(err);
+      } else {
+        cb(null, hash);
+      }
+    });
+  });
+};
 
 router.post('/', function(req, res) {
   if (!req.body.email || !validator.isEmail(req.body.email)) {
@@ -18,14 +31,23 @@ router.post('/', function(req, res) {
       'error': 'invalid_password'
     });
   } else {
-    models.User.create({
-      email: req.body.email,
-      password: req.body.password
-    }).then(function() {
-      res.header('Location', config.host + config.login.redirect_path.success);
-      res.status(201).json({
-        'ok': true
-      });
+    getPasswordHash(req.body.password, function(err, hash) {
+      if (err) {
+        res.json({
+          'ok': false,
+          'error': 'invalid_password'
+        });
+      } else {
+        models.User.create({
+          email: req.body.email,
+          password: hash
+        }).then(function() {
+          res.header('Location', config.host + config.login.redirect_path.success);
+          res.status(201).json({
+            'ok': true
+          });
+        });
+      }
     });
   }
 });
