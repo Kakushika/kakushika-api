d7fce37fb35c2f5a5fc62c88739a84434e10ef08
Orleika <orleika.net@gmail.com>
2015-08-15 20:25:04 +0900

    テストコードの構造を変更

--- test/api/callback.js
+++ test/api/callback.js
@@ -2,20 +2,20 @@
 
 var expect = require('chai').expect,
   supertest = require('supertest'),
-  app = require('../app');
+  app = require('../../app');
 
 describe('Callback', function() {
   it('should return a 302 response', function(done) {
     supertest(app)
       .get('/callback/slack')
-      .set('X-Requested-With', 'XMLHttpRequest')
-      .expect(302)
+      .set('x-access-token', global.accessToken)
+      .expect(200)
       .end(function(err, res) {
         if (err) {
           return done(err);
         }
-        expect(res.header).to.have.property('location');
-        expect(res.header.location).not.to.be.null;
+        expect(res.body).to.have.property('ok');
+        expect(res.body.result).to.not.be.true;
         done();
       });
   });
---
+++ test/api/login.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var expect = require('chai').expect,
+  supertest = require('supertest'),
+  app = require('../../app');
+
+describe('Login', function() {
+  it('should return a 400 response', function(done) {
+    supertest(app)
+      .post('/login')
+      .expect(400, done);
+  });
+
+  it('should return a 200 response', function (done) {
+    supertest(app)
+      .post('/login')
+      .send({
+        'email': 'hoge@hoge.net',
+        'password': 'hoge'
+      })
+      .expect(200)
+      .end(function(err, res) {
+        if (err) {
+          return done(err);
+        }
+        expect(res.body).to.have.property('ok');
+        expect(res.body.ok).to.be.true;
+        expect(res.body).to.have.property('token');
+        expect(res.body.token).not.to.be.null;
+        done();
+      });
+  });
+});
