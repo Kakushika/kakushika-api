99cfe4d308fc2e711eb189d1e436099dd5a9f0c2
Orleika <orleika.net@gmail.com>
2015-08-17 17:10:54 +0900

    ストラテジーの除去

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,11 @@
 
 var fs = require('fs'),
   express = require('express'),
-  passport = require('passport'),
+  router = express.Router(),
   jwt = require('jsonwebtoken'),
   config = require('config'),
-  router = express.Router();
+  validator = require('validator'),
+  models = require('../models');
 
 fs.readdirSync(__dirname + '/').forEach(function(file) {
   if (~file.indexOf('.js') && !~file.indexOf('index')) {
@@ -14,18 +15,53 @@ fs.readdirSync(__dirname + '/').forEach(function(file) {
   }
 });
 
-router.post('/login', passport.authenticate('local', {
-  session: false
-}), function(req, res) {
-  var token = jwt.sign({
-    id: req.user.id
-  }, config.jwt.secret, {
-    expiresInMinutes: 1440 // 24 hours
-  });
-  res.json({
-    ok: true,
-    token: token
-  });
+router.post('/login', function(req, res, next) {
+  var email = req.body.email,
+    password = req.body.password;
+
+  if (!email || !validator.isEmail(email)) {
+    res.json({
+      ok: false,
+      message: 'invalid_email'
+    });
+  } else if (!password) {
+    res.json({
+      ok: false,
+      message: 'invalid_password'
+    });
+  } else {
+    models.User.findOne({
+      email: email
+    }).then(function(user) {
+      if (!user) {
+        return res.json({
+          ok: false
+        });
+      }
+      user.verifyPassword(password, function(err, result) {
+        if (err) {
+          return next(err);
+        }
+        if (!result) {
+          return res.json({
+            ok: false
+          });
+        } else {
+          var token = jwt.sign({
+            id: user.id
+          }, config.jwt.secret, {
+            expiresInMinutes: 1440 // 24 hours
+          });
+          res.json({
+            ok: true,
+            token: token
+          });
+        }
+      });
+    }).catch(function(err) {
+      return next(err);
+    });
+  }
 });
 
 router.get('/', function(req, res) {
