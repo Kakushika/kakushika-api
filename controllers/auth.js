dfc13edca584913ba9eed79cf8cb151a3375fcae
Yoshiyuki Taniguchi <iwatesoftware@live.jp>
2015-08-19 23:47:25 +0900

    change path

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,7 +8,7 @@ var express = require('express'),
 router.get('/slack', function(req, res) {
   Slack.oauth.getUrl({
     client_id: config.slack.client_id,
-    redirect_uri: config.api.host + config.slack.callback_path
+    redirect_uri: config.host + config.slack.callback_path
   }, function(error, url) {
     if (error) {
       res.redirect(config.host + config.slack.redirect_path.error);
