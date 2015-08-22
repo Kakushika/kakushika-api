0558237089ba3a9a055191abfb59bcb0db1aeabe
Orleika <orleika.net@gmail.com>
2015-08-22 19:12:12 +0900

    MessageProperty modelの修正

--- models/message.js
+++ models/message.js
@@ -12,6 +12,14 @@ module.exports = function(sequelize, DataTypes) {
     message: DataTypes.TEXT,
     messagePropertyId: DataTypes.INTEGER,
     pubDate: DataTypes.DATE
+  }, {
+    classMethods: {
+      associate: function(models) {
+        Message.hasOne(models.MessageProperty, {
+          foreignKey: 'messageId'
+        });
+      }
+    }
   });
 
   return Message;
