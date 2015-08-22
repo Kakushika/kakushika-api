909e21960ef63c4c36f8f3f48eaf408ed9354418
Orleika <orleika.net@gmail.com>
2015-08-23 02:51:54 +0900

    room apiの変更

--- controllers/room.js
+++ controllers/room.js
@@ -8,22 +8,35 @@ var express = require('express'),
 router.get('/', auth, function(req, res, next) {
   var userId = req.decoded.id;
 
-  models.Room.findAll({
-    attributes: [
-      'id',
-      'name',
-      'externalType',
-      'externalId',
-      'roomGroupId',
-      'latest'
-    ],
-    where: {
-      userId: userId
-    }
-  }).then(function(rooms) {
-    res.json({
+  models.User.findById(userId, {
+    include: [{
+      model: models.Room,
+      as: 'ReadableRooms',
+      attributes: [
+        'id',
+        'name',
+        'externalType',
+        'externalId',
+        'roomGroupId',
+        'latest'
+      ]
+    }, {
+      model: models.Room,
+      as: 'Rooms',
+      attributes: [
+        'id',
+        'name',
+        'externalType',
+        'externalId',
+        'roomGroupId',
+        'latest'
+      ]
+    }]
+  }).then(function(user) {
+    return res.json({
       ok: true,
-      rooms: rooms
+      rooms: user.Rooms,
+      readableRooms: user.ReadableRooms
     });
   }).catch(function(err) {
     next(err);
@@ -40,7 +53,7 @@ router.get('/:external_id', auth, function(req, res, next) {
       as: 'ExternalUsers'
     }
   }).then(function(room) {
-    if(room.userId !== userId) {
+    if (room.userId !== userId) {
       return res.status(400).json({
         ok: false
       });
