daa2d2442f70a84eaf2f68ef648e8423769edba6
Orleika <orleika.net@gmail.com>
2015-08-22 19:46:48 +0900

    スケジューラーをきれいに

--- utils/task.js
+++ utils/task.js
@@ -5,7 +5,7 @@ var fs = require('fs'),
   schedulerManagement = require('azure-scheduler'),
   date = require('./date');
 
-module.exports = function(user_id, room_id) {
+var connect = function(callback) {
   fs.readFile(__dirname + '/' + config.subscriptionId + '.pem', 'utf8', function(err, pem) {
     if (err) {
       console.error(err);
@@ -16,24 +16,33 @@ module.exports = function(user_id, room_id) {
       pem: pem
     });
     var client = schedulerManagement.createSchedulerClient(config.cloudService, config.taskname, credentials, config.url);
-
-    client.jobs.createOrUpdate(room_id, {
-      startTime: date.getTimeStamp(),
-      action: {
-        type: 'storageQueue',
-        queueMessage: {
-          storageAccountName: 'kksk',
-          queueName: 'task',
-          sasToken: config.sasToken,
-          message: '{ "user": ' + user_id + ', "room": ' + room_id + ', "external": "slack" }'
-        }
-      },
-      recurrence: config.recurrence
-    }, function(err, result) {
-      if (err) {
-        console.error(err);
-      }
-      console.log(result);
-    });
+    callback(client);
   });
 };
+
+var task = {
+  create: function(user_id, room_id) {
+    connect(function(client) {
+      client.jobs.createOrUpdate(room_id, {
+        startTime: date.getTimeStamp(),
+        action: {
+          type: 'storageQueue',
+          queueMessage: {
+            storageAccountName: 'kksk',
+            queueName: 'task',
+            sasToken: config.sasToken,
+            message: '{ "user": ' + user_id + ', "room": ' + room_id + ', "external": "slack" }'
+          }
+        },
+        recurrence: config.recurrence
+      }, function(err, result) {
+        if (err) {
+          console.error(err);
+        }
+        console.log(result);
+      });
+    });
+  }
+};
+
+module.exports = task;
