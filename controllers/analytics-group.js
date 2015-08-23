12b8c68214014daf09cf652df7f82740cd0a0104
Orleika <orleika.net@gmail.com>
2015-08-23 12:17:26 +0900

    analytics goup apiの追加

--- controllers/analytics-group.js
+++ controllers/analytics-group.js
@@ -65,20 +65,149 @@ router.get('/:analyticsGroupId', auth, function(req, res, next) {
   });
 });
 
-router.post('/create', auth, function(req, res, next) {
-  var userId = req.decoded.id;
-  var name = req.body.name;
+router.post('/', auth, function(req, res, next) {
+  var userId = req.decoded.id,
+    name = req.body.name;
 
   models.AnalyticsGroup.create({
     userId: userId,
     name: name
   }).then(function(analyticsGroup) {
-    res.json({
+    res.status(201).son({
       ok: true,
       analyticsGroup: analyticsGroup
     });
   }).catch(function(err) {
     next(err);
+  });
+});
+
+router.put('/:analytics_group_id/rooms', auth, function(req, res, next) {
+  var userId = req.decoded.id,
+    analyticsGroupId = req.params.analytics_group_id,
+    roomId = req.body.roomId;
+
+  models.AnalyticsGroup.findById(analyticsGroupId, {
+    where: {
+      userId: userId
+    }
+  }).then(function(analyticsGroup) {
+    if (!analyticsGroup) {
+      return res.status(401).json({
+        ok: false
+      });
+    }
+    models.Room.findById(roomId, {
+      where: {
+        userId: userId
+      }
+    }).then(function(room) {
+      if (!room) {
+        return res.status(401).json({
+          ok: false
+        });
+      }
+      models.AnalyticsGroupRoom.create({
+        analyticsGroupId: analyticsGroupId,
+        roomId: roomId
+      }).then(function(analyticsGroupRoom) {
+        return res.json({
+          ok: true,
+          analyticsGroupRoom: analyticsGroupRoom
+        });
+      }).catch(function(err) {
+        return next(err);
+      });
+    }).catch(function(err) {
+      return next(err);
+    });
+  }).catch(function(err) {
+    return next(err);
+  });
+});
+
+router.put('/:analytics_group_id/groups', auth, function(req, res, next) {
+  var userId = req.decoded.id,
+    analyticsGroupId = req.params.analytics_group_id,
+    groupId = req.body.group_id;
+
+  models.AnalyticsGroup.findById(analyticsGroupId, {
+    where: {
+      userId: userId
+    }
+  }).then(function(analyticsGroup) {
+    if (!analyticsGroup) {
+      return res.status(401).json({
+        ok: false
+      });
+    }
+    models.AnalyticsGroup.findById(groupId, {
+      where: {
+        userId: userId
+      }
+    }).then(function(group) {
+      if (!group) {
+        return res.status(401).json({
+          ok: false
+        });
+      }
+      models.AnalyticsGroupMesh.create({
+        parentAnalyticsGroupId: analyticsGroupId,
+        childAnalyticsGroupId: groupId
+      }).then(function(analyticsGroupMesh) {
+        return res.json({
+          ok: true,
+          analyticsGroupMesh: analyticsGroupMesh
+        });
+      }).catch(function(err) {
+        return next(err);
+      });
+    }).catch(function(err) {
+      return next(err);
+    });
+  }).catch(function(err) {
+    return next(err);
+  });
+});
+
+router.put('/:analytics_group_id/users', auth, function(req, res, next) {
+  var userId = req.decoded.id,
+    analyticsGroupId = req.params.analytics_group_id,
+    addUserId = req.body.user_id;
+
+  models.AnalyticsGroup.findById(analyticsGroupId, {
+    where: {
+      userId: userId
+    }
+  }).then(function(analyticsGroup) {
+    if (!analyticsGroup) {
+      return res.status(401).json({
+        ok: false
+      });
+    }
+    models.User.findById(addUserId)
+      .then(function(user) {
+        if (!user) {
+          return res.status(404).json({
+            ok: false
+          });
+        }
+        models.AnalyticsUser.create({
+          userId: addUserId,
+          analyticsGroupId: analyticsGroupId
+        }).then(function(analyticsUser) {
+          return res.json({
+            ok: true,
+            analyticsUser: analyticsUser
+          });
+        }).catch(function(err) {
+          return next(err);
+        });
+      }).catch(function(err) {
+        return next(err);
+      });
+  }).catch(function(err) {
+    return next(err);
   });
 });
 
