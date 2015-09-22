aa0f7918de62094d784a845a69745c421ab10557
Orleika <orleika.net@gmail.com>
2015-09-22 17:32:32 +0900

    認可ミドルウエアの追加

---
+++ middleware/authorization.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var jwt = require('jsonwebtoken'),
+  config = require('config');
+
+module.exports = function(req, res, next) {
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+  jwt.verify(token, config.jwt.secret, function(err, decoded) {
+    if (err) {
+      return res.status(401).json({
+        ok: false,
+        message: 'Failed to authenticate token.'
+      });
+    } else {
+      req.decoded = decoded;
+      next();
+    }
+  });
+};
