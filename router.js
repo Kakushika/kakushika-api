0cd5fcb9ba0af94c61e62acc45420841d517893f
Orleika <orleika.net@gmail.com>
2015-10-10 21:47:45 +0900

    routerの更新

--- router.js
+++ router.js
@@ -14,22 +14,29 @@ router.get('/', function(req, res) {
 router.post('/signup', controllers.authentication.signup);
 router.post('/login', controllers.authentication.login);
 
-router.get('/users', authorization, controllers.users.read);
-router.get('/me', authorization, controllers.users.me);
+// require authentication area
+router.use(authorization);
 
-router.get('/connect/slack', authorization, controllers.connect.slack.oauth);
-router.post('/connect/slack', authorization, controllers.connect.slack.callback);
+router.get('/me', controllers.users.me);
+router.get('/users', controllers.users.read);
 
-router.post('/folders', authorization, controllers.folders.create);
-router.get('/folders/:folder_id', authorization, controllers.folders.read);
-router.post('/folders/:folder_id/children', authorization, controllers.folders.createChildren);
-router.post('/folders/:folder_id/reader', authorization, controllers.folders.reader);
-router.get('/resolve', authorization, controllers.folders.resolve);
-router.post('/rooms/:room_id/reader', authorization, controllers.rooms.reader);
+router.get('/connect/slack', controllers.connect.slack.oauth);
+router.post('/connect/slack', controllers.connect.slack.callback);
+router.post('/connect/hipchat', controllers.connect.hipchat);
 
-router.get('/messages', authorization, controllers.messages.search);
+router.post('/folders', controllers.folders.create);
+router.get('/folders/:folder_id', controllers.folders.read);
+router.post('/folders/:folder_id/children', controllers.folders.createChildren);
+router.delete('/folders/:folder_id/children', controllers.folders.deleteChildren);
+router.post('/folders/:folder_id/reader', controllers.folders.createReader);
+router.delete('/folders/:folder_id/reader', controllers.folders.deteleteReader);
+router.get('/folders/resolve', controllers.folders.resolve);
 
-router.get('/assets', authorization, controllers.assets.search);
-router.get('/assets/types', authorization, controllers.assets.type);
+router.get('/rooms/:room_id/messages', controllers.rooms.reader);
+router.post('/rooms/:room_id/reader', controllers.rooms.createReader);
+router.delete('/rooms/:room_id/reader', controllers.rooms.deleteReader);
+
+router.get('/messages', controllers.search.messages);
+router.get('/assets', controllers.search.assets);
 
 module.exports = router;
